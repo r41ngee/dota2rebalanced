45e7e3773c581f47f4cd0233cc32f3d17cfbde2b
r41ngee <146991131+r41ngee@users.noreply.github.com>
2025-10-02 17:47:24 +0300

    Delete update.ps1

--- update.ps1
+++
@@ -1,5 +0,0 @@
-$contentPath = "G:\SteamLibrary\steamapps\common\dota 2 beta\content\dota_addons\dota_rebalanced\*"
-$gamePath = "G:\SteamLibrary\steamapps\common\dota 2 beta\game\dota_addons\dota_rebalanced\*"
-
-Copy-Item -Path $contentPath -Destination "./content" -Force -Recurse;
-Copy-Item -Path $gamePath -Destination "./game" -Force -Recurse;
