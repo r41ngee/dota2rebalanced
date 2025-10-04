cade1e7e7dc1ac4f2e2215e67af9589df88871b1
r41ngee <r41ngee@yandex.ru>
2025-10-04 13:01:54 +0300

    lina:changed shard

--- game/scripts/vscripts/ability/lina_overheat.lua
+++ game/scripts/vscripts/ability/lina_overheat.lua
@@ -1,4 +1,22 @@
 lina_overheat_nw = class({})
+function lina_overheat_nw:GetBehavior()
+    if self:GetCaster():HasModifier("modifier_item_aghanims_shard") then
+        return DOTA_ABILITY_BEHAVIOR_IMMEDIATE + DOTA_ABILITY_BEHAVIOR_NO_TARGET
+    end
+    return DOTA_ABILITY_BEHAVIOR_PASSIVE
+end
+
+function lina_overheat_nw:OnSpellStart()
+    local caster = self:GetCaster()
+    caster:AddNewModifier(
+        caster,
+        self,
+        "modifier_lina_overheat_nw_building",
+        {
+            duration = self:GetSpecialValueFor("building_damage_duration")
+        }
+    )
+end
 
 modifier_lina_overheat_nw = class({})
 function modifier_lina_overheat_nw:IsHidden() return true end
@@ -36,20 +54,29 @@ function modifier_lina_overheat_nw:OnAttackLanded(event)
 
     if parent:GetTeamNumber() == victim:GetTeamNumber() then return end
     if parent:IsIllusion() then return end
-    if victim:IsBuilding() then return end
+
+    local damage_multiplier = 1
+    if victim:IsBuilding() then
+        if parent:HasModifier("modifier_lina_overheat_nw_building") then
+            damage_multiplier = ability:GetSpecialValueFor("building_damage") / 100
+        else
+            return
+        end
+    end
 
     local mark_modifier = victim:FindModifierByName("modifier_lina_overheat_nw_stack")
 
     if mark_modifier then
         if mark_modifier:GetStackCount() >= ability:GetSpecialValueFor("required_stacks") then
+            local final_damage = ability:GetSpecialValueFor("damage") * damage_multiplier
             ApplyDamage({
                 victim = victim,
                 attacker = parent,
-                damage = ability:GetSpecialValueFor("damage"),
+                damage = final_damage,
                 damage_type = ability:GetAbilityDamageType(),
                 ability = ability
             })
-            SendOverheadEventMessage(victim, 4, victim, ability:GetSpecialValueFor("damage"), nil)
+            SendOverheadEventMessage(victim, 4, victim, final_damage, nil)
             mark_modifier:Destroy()
         else
             mark_modifier:IncrementStackCount()
@@ -64,5 +91,11 @@ function modifier_lina_overheat_nw:OnAttackLanded(event)
     end
 end
 
+modifier_lina_overheat_nw_building = class({})
+function modifier_lina_overheat_nw_building:IsHidden() return false end
+function modifier_lina_overheat_nw_building:IsPurgable() return false end
+function modifier_lina_overheat_nw_building:IsDebuff() return false end
+
 LinkLuaModifier("modifier_lina_overheat_nw", "ability/lina_overheat.lua", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_lina_overheat_nw_stack", "ability/lina_overheat.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_lina_overheat_nw_building", "ability/lina_overheat.lua", LUA_MODIFIER_MOTION_NONE)
