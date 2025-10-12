895df853ebdc730dfb39b84072d6a991da117d12
r41ngee <r41ngee@yandex.ru>
2025-10-12 14:07:04 +0300

    fixed higher chance + debug

--- game/scripts/vscripts/prd.lua
+++ game/scripts/vscripts/prd.lua
@@ -1,8 +1,14 @@
 function PRDCalc(p, att)
+    DebugPrint("prd: nominal = " .. p)
     local N = math.floor(1/p)
     local C = 1 - (1-p)^(1/N)
+    DebugPrint("prd: c_const = " .. C)
 
-    return RandomFloat(0, 1) < C * (att + 1)
+    local final = C * (att)
+    DebugPrint("prd: attempt = " .. att)
+    DebugPrint("prd: factical = " .. final)
+
+    return RandomFloat(0, 1) < final
 end
 
 function PRDCalc_Pct(p, att)
