44dfa94c87aa0a79a117a10fe040137f97e16cf9
r41ngee <r41ngee@yandex.ru>
2025-10-29 20:44:28 +0300

    reworked facet innate

--- game/scripts/vscripts/ability/morphling_bubble.lua
+++ game/scripts/vscripts/ability/morphling_bubble.lua
@@ -17,6 +17,8 @@ end
 
 function modifier_morphling_bubble:DeclareFunctions()
     return {
+        MODIFIER_EVENT_ON_ABILITY_FULLY_CAST,
+        MODIFIER_PROPERTY_BASEATTACK_BONUSDAMAGE,
         MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE
     }
 end
@@ -24,7 +26,15 @@ end
 function modifier_morphling_bubble:GetModifierMoveSpeedBonus_Percentage()
     local parent = self:GetParent()
     local ability = self:GetAbility()
-    return math.min(parent:GetAgility() / ability:GetSpecialValueFor("agi_per_ms"), ability:GetSpecialValueFor("ms_limit_pct"))
+    return math.min(math.max(0, (parent:GetAgility() - parent:GetStrength()) * ability:GetSpecialValueFor("ms_pct_agi")), ability:GetSpecialValueFor("max_ms_pct"))
+end
+
+function modifier_morphling_bubble:GetModifierBaseAttack_BonusDamage()
+    return self:GetAbility():GetSpecialValueFor("bonus_base_attack")
+end
+
+function modifier_morphling_bubble:OnAbilityFullyCast()
+    self:OnCreated()
 end
 
 LinkLuaModifier("modifier_morphling_bubble", "ability/morphling_bubble.lua", LUA_MODIFIER_MOTION_NONE)
